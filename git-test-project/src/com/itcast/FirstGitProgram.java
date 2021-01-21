82cfef08f957ab0cf035d16c3986090be5f9e550
许传军 <cnxuchuanjun@163.com>
2021-01-21 20:24:53 +0800

    从github远程仓库克隆之后，进行的第一次修改

--- git-test-project/src/com/itcast/FirstGitProgram.java
+++ git-test-project/src/com/itcast/FirstGitProgram.java
@@ -10,11 +10,14 @@ public class FirstGitProgram {
         System.out.println("Hello git project!");
         System.out.println("Hello git project!");
         System.out.println("Hello git project!");
+        //这一行是克隆之后新增加的
         System.out.println("Hello git project!");
     }
 
     public static void test(){
         int i=0;
         i+=10;
+        //这一行是克隆之后新增加的
+        System.out.println(i);
     }
 }
