1fed585de8570372deba78b5c47abce4b334b4ac
许传军 <cnxuchuanjun@163.com>
2021-01-21 19:55:46 +0800

    本地工程添加到git本地仓库

---
+++ git-test-project/src/com/itcast/FirstGitProgram.java
@@ -0,0 +1,15 @@
+package com.itcast;
+
+public class FirstGitProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+        System.out.println("Hello git project!");
+    }
+}
